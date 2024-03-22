b909c0eebf95cf6460540f404788f26dbfc6e83d
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2024-03-22 19:17:13 +0000

    add dark mode and toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Header from "./components/Header/Header"
+
 const App = () => {
-  return <h1>Hello Tailwind</h1>
+  return <Header />
 }
 
 export default App
---
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -0,0 +1,20 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid"
+import { useEffect } from "react"
+
+const ToggleTheme = () => {
+    const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const pageClasses = document.documentElement.classList
+
+    useEffect(() => {
+        systemPreference && pageClasses.add('dark')
+    })
+    const toggle = () => {
+        pageClasses.toggle('dark')
+    }
+    return <div className="hidden sm:block">
+        <MoonIcon className="h-8 text-gray-100 block dark:hidden cursor-auto" onClick={toggle} />
+        <SunIcon className="h-8 text-gray-100 hidden dark:block cursor-pointer" onClick={toggle} />
+    </div>
+}
+
+export default ToggleTheme
